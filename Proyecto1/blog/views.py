0b1b21fe8740951aba176d9d5b1221ca598b6c09
Daniel <josetisrey@gmail.com>
2021-07-25 13:39:39 -0600

    hola mundo-Proyecto1

---
+++ Proyecto1/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hola(request):
+    return HttpResponse("Hola mundo")
+
+def comoEsta(request):
+    return HttpResponse("como esta")
